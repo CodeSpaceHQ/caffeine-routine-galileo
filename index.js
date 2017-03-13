6312eb8325bf3bc948b0c4a818014a4215fcaea5
Alexander Clines <injpk23@gmail.com>
2017-03-12 21:36:45 -0500

    Added body parsing

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World! - Caffeine Routine')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+  console.log(req.body);
+  res.send(req.body)
 })
 
 app.listen(3000, function () {
