87c022df0f6abbd1558a00d6887ffc3950a67e67
Alexander Clines <injpk23@gmail.com>
2017-03-31 10:24:51 -0500

    Added inline documentation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const Keurig = require('./keurig.js');
-const lcd = require('./lcd.js');
+const express = require('express'); // Express server
+const bodyParser = require('body-parser'); // JSON body parser
+const Keurig = require('./keurig.js'); // Keurig emulator
+const lcd = require('./lcd.js'); // Displaying text on LCD screen
 
-const app = express();
-const keurig = new Keurig();
+const app = express(); // The actual express application
+const keurig = new Keurig(); // The keurig emulator
 
+
+// Prepares URL encoding for a JSON body
 app.use(bodyParser.urlencoded({
   extended: false,
 }));
 
+// Tells the applicatoin to expect a JSON body in request
 app.use(bodyParser.json());
 
+// Displays a message to the LCD screen
 app.post('/', (req, res) => {
   lcd.displayMessage({
     message: req.body.message,
@@ -22,11 +26,13 @@ app.post('/', (req, res) => {
   res.send(req.body);
 });
 
+// Heat up water
 app.post('/heat', (req, res) => {
   keurig.heatUp();
   res.send('Heating up');
 });
 
+// Brew coffee
 app.post('/brew', (req, res) => {
   keurig.brew(req.body.size, (err) => {
     if (err) res.status(400).send(err.message);
@@ -34,15 +40,20 @@ app.post('/brew', (req, res) => {
   });
 });
 
+
+// Retrieve schedule
 app.get('/schedule', (req, res) => {
   res.send(keurig.getSchedule());
 });
 
+// Update schedule
 app.post('/schedule', (req, res) => {
   keurig.setSchedule(req.body.schedule);
   res.send('Schedule set');
 });
 
+// Set server to listen to port
 const server = app.listen(3000);
 
+// So server can be externally accessed for testing
 module.exports = server;
